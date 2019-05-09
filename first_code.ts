99764a6acbf30212db9ff37cc313e078767c6f3f
Marcos Daniel <marcos.mdbbr@gmail.com>
2019-05-09 17:08:38 -0300

    first code

---
+++ first_code.ts
@@ -0,0 +1,13 @@
+//with js
+function sayHello(nome){
+    console.log('Hello' + name);
+}
+
+sayHello('Marcos');
+
+// with ts
+function sayNewHello(nome: string){
+    console.log('Hello '+ nome + ', how are you?');
+}
+
+sayNewHello('Marcos Daniel');
