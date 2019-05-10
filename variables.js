259b589f64197f086687d48ba434412a0846710e
Marcos Daniel <marcos.mdbbr@gmail.com>
2019-05-10 09:07:13 -0300

    working with variables - var and let

---
+++ variables.js
@@ -0,0 +1,18 @@
+//with js 
+function show() {
+    var text = 'Marcos Daniel';
+    if (true) {
+        var text = 'text for example';
+    }
+    console.log(text);
+}
+show();
+//with ts
+function showWithLet() {
+    var text = 'Marcos Daniel';
+    if (true) {
+        var text_1 = 'text for example';
+    }
+    console.log(text);
+}
+showWithLet();
---
+++ variables.ts
@@ -0,0 +1,21 @@
+//with js 
+function show(){
+    var text = 'Marcos Daniel';
+    if(true){
+        var text = 'text for example';
+    }
+    console.log(text);
+}
+
+show();
+
+//with ts
+function showWithLet(){
+    let text = 'Marcos Daniel';
+    if(true){
+        let text = 'text for example';
+    }
+    console.log(text);
+}
+
+showWithLet();
