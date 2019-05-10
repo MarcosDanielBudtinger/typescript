8842892907f7235c1ccad98a8bb2fb515de325fa
Marcos Daniel <marcos.mdbbr@gmail.com>
2019-05-10 14:06:23 -0300

    type object and type any with ts

---
+++ type_object_e_any.ts
@@ -0,0 +1,18 @@
+
+//object
+let obj: object = {
+    first_name: 'Marcos Daniel',
+    last_name: 'Budtinger',
+    age: 29,
+    city: 'Capanema',
+    estate: 'PR'
+}
+
+//any
+let variable: any = 4;
+
+variable = 'quatro';
+variable = false;
+
+let myArray: Array<any> = [1,2,3,'quatro',false, {city: 'Capanema'}];
+
